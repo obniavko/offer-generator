c5845532ca3d26491df32e3a68effc4424b434a1
obniavko <vobniavko@gmail.com>
2024-01-23 19:42:52 +0200

    feat: update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  root 'offers#index'
+  root "home#index"
 
   devise_for :users
-  resources :offers
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
